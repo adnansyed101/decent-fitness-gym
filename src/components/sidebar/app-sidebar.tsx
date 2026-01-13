a580e78d8b3869d2a4d0f53c1516f6aa4723f375
adnansyed101 <sirgeant10@gmail.com>
2026-01-13 16:45:04 +0600

    FIx sidebar header overflow.

--- src/components/sidebar/app-sidebar.tsx
+++ src/components/sidebar/app-sidebar.tsx
@@ -6,6 +6,7 @@ import {
   SidebarContent,
   SidebarFooter,
   SidebarHeader,
+  SidebarMenuButton,
   SidebarRail,
 } from '@/components/ui/sidebar'
 import { Links } from '@/lib/types/general.types'
@@ -28,17 +29,22 @@ export const AppSidebar: React.FC<AppSidebarProps> = ({ links, ...props }) => {
   return (
     <Sidebar collapsible="icon" {...props}>
       <SidebarHeader className="bg-[#111318] border-b">
-        <div className="flex items-center gap-3 py-2">
-          <div className="w-10 h-10 rounded-xl bg-gradient-primary flex items-center justify-center shadow-glow">
-            <Dumbbell className="w-5 h-5 text-primary-foreground" />
+        <SidebarMenuButton
+          size="lg"
+          className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+        >
+          <div className="flex items-center gap-3 py-2">
+            <div className="p-1 rounded-xl bg-gradient-primary flex items-center justify-center shadow-glow">
+              <Dumbbell className="w-5 h-5 text-primary-foreground" />
+            </div>
+            <div>
+              <h1 className="font-display font-bold text-lg text-foreground">
+                Decent Fitness Gym
+              </h1>
+              <p className="text-xs text-muted-foreground">Gym Management</p>
+            </div>
           </div>
-          <div>
-            <h1 className="font-display font-bold text-lg text-foreground">
-              Decent Fitness Gym
-            </h1>
-            <p className="text-xs text-muted-foreground">Gym Management</p>
-          </div>
-        </div>
+        </SidebarMenuButton>
       </SidebarHeader>
       <SidebarContent className="bg-[#111318]">
         {links.map((link) => (
